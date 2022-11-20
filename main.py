60112c5c5d6b39032577c864e2d33699e0181a58
insanolanbiri <erenakgun2007@hotmail.com>
2022-11-20 14:56:23 +0300

    update

--- main.py
+++ main.py
@@ -1,22 +1,40 @@
 from os import getenv
 from time import time
+from typing import Any, TypeVar
 from uuid import uuid1
 
 from dotenv import load_dotenv
 from eokulapi.eokulapi import EokulAPI
 from eokulapi.Models.MarkLesson import MarkLesson
 from feedgen.feed import FeedGenerator
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, render_template, url_for
 
 load_dotenv()
 emptylesson = MarkLesson(None, None, None, None, None, None, None, None, {}, {})
 
-list_difference = lambda l1, l2: [x for x in l1 if x not in l2]
+
+T = TypeVar("T")
 
 
-def dict_difference(d1: dict, d2: dict) -> set:
+def list_difference(l1: list[T], l2: list[T]) -> list[T]:
+    return [x for x in l1 if x not in l2]
+
+
+def dict_difference(d1: dict[T, Any], d2: dict[T, Any]) -> set[T]:
     return (set(d1) - set(d2)).union(set(d2) - set(d1))
 
+
+CACHING_TIME: float = 60.0
+"""cache timeout in seconds"""
+
+last_update = 0.0
+
+app = Flask(__name__)
+
+user = EokulAPI(uid=getenv("EOKUL_UID", ""))
+st = user.students[0]
+user._update_marks(st)
+state = st.marks.data if not getenv("APP_TEST_MSG") else []
 
 hostname = getenv("APP_HOSTNAME", "http://localhost:5000")
 fg = FeedGenerator()
@@ -29,15 +47,6 @@ fg.image(f"{hostname}/static/little_logo.png")
 fg.logo(f"{hostname}/static/little_logo.png")
 fg.subtitle("bir okulun bir sınıfının sınavlarının açıklanıp açıklanmadığı ve sınıf ortalaması")
 fg.language("tr")
-
-app = Flask(__name__)
-
-
-user = EokulAPI(uid=getenv("EOKUL_UID", ""))
-st = user.students[0]
-user._update_marks(st)
-state = st.marks.data
-last_update = 0.0
 
 
 @app.route("/")
@@ -62,22 +71,11 @@ def send_exam_result_atom():
     return fg.atom_str()
 
 
-@app.route("/feeds/sinav_sonuc_rss.xml", methods=["GET"])
-def send_exam_result_rss():
-    mesg = check_exam_result()
-    if mesg:
-        for i in mesg:
-            fe = fg.add_entry()
-            fe.title(i[0])
-            fe.description(i[1])
-            fe.id(uuid1().hex)
-    return fg.rss_str()
-
-
 def check_exam_result() -> list[tuple[str]]:
     global state
     global last_update
-    if not time() - last_update > 60:
+    global CACHING_TIME
+    if not time() - last_update > CACHING_TIME:
         return []
     last_update = time()
     user._update_marks(st)
@@ -87,36 +85,32 @@ def check_exam_result() -> list[tuple[str]]:
     newstate = st.marks.data
     diff = list_difference(newstate, state)
     result = []
-    for i in diff:
-        changed_lesson: MarkLesson = i
+    for changed_lesson in diff:
         old_changed_lessons = [
-            lesson for lesson in state if lesson.lesson_id == changed_lesson.lesson_id
+            l
+            for l in state
+            if (l.lesson_id == changed_lesson.lesson_id and l.term == changed_lesson.term)
         ]
-        old_changed_lesson: MarkLesson = (
-            old_changed_lessons[0] if old_changed_lessons else emptylesson
-        )
-        ders = changed_lesson.lesson
+        old_changed_lesson = old_changed_lessons[0] if old_changed_lessons else emptylesson
         sozlu_diff = dict_difference(old_changed_lesson.sozlu, changed_lesson.sozlu)
         yazili_diff = dict_difference(old_changed_lesson.yazili, changed_lesson.yazili)
         for nth in yazili_diff:
-            dtype = "yazılı"
-            for d in st.class_exam_average.data:
-                if d.lesson_name == ders:
-                    ort = d.marks[nth].avg_mark
-                    break
+            ort = [
+                d.marks[nth].avg_mark
+                for d in st.class_exam_average.data
+                if (d.lesson_name == changed_lesson.lesson and d.term == changed_lesson.term)
+            ]
             result.append(
                 (
-                    f"{ders.replace('I','ı').lower()} {nth}. {dtype} açıklanmış",
-                    f"sınıf ortalaması: {ort}",
+                    f"{changed_lesson.mark_to_str(True,nth)} açıklanmış",
+                    f"sınıf ortalaması: {ort[0] if ort else 'tuhaf bir şekilde belirsiz!?'}",
                 )
             )
         for nth in sozlu_diff:
-            dtype = "sözlü"
-            ort = "belirsiz"
             result.append(
                 (
-                    f"{ders.replace('I','ı').lower()} {nth}. {dtype} açıklanmış",
-                    f"sınıf ortalaması: {ort}",
+                    f"{changed_lesson.mark_to_str(False,nth)} açıklanmış",
+                    f"sınıf ortalaması: belirsiz",
                 )
             )
     state = newstate
